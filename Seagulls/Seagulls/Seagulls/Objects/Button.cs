ece245b9935a391ec3d5b749a368a8462cc778b3
Mei <cirno@live.nl>
2013-09-02 11:55:33 +0200

    update to beta1 playabale

--- Seagulls/Seagulls/Seagulls/Objects/Button.cs
+++ Seagulls/Seagulls/Seagulls/Objects/Button.cs
@@ -5,11 +5,24 @@ using System.Text;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace Seagulls
 {
     class Button : Sprite
     {
+        enum Bstate { IDLE, CLICKED, DISABLED }
+        const int BUTTON_COUNT = 1,
+                  BTN_GAME_QUIT = 401,
+                  BTN_GAME_PAUSE = 402,
+                  BTN_MENU_START = 403,
+                  BTN_MENU_OPTIONS = 404,
+                  BTN_MENU_EXIT = 405,
+                  BTN_SCORE_RETRY = 406,
+                  BTN_SCORE_RETURN = 407,
+                  BTN_OPTIONS_MUSIC_ON = 408,
+                  BTN_OPTIONS_MUSIC_OFF = 409;
+        
         private string button_ASSETNAME;
 
         public void LoadContent(ContentManager theContentManager, string theAssetName, int x, int y) 
